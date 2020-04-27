5f0febfdf7392a1c4f500b9d9143a9687a5dfc6d
Zion Perez <zionperez@gmail.com>
2020-04-26 21:30:15 -0600

    Added decode, encode, and translate functions to starter2.

--- starter_code2.py
+++ starter_code2.py
@@ -1,6 +1,45 @@
 #!/usr/bin/python3
 import argparse
 import socket
+import base64
+
+def decodeToBinary(source, format):
+    if format == "raw":
+        return bin(int.from_bytes(source.encode(), 'big'))
+    if format == "b64":
+        return base64.b64decode(source)
+    if format == "hex":
+        return bin(int(source, 16))[2:].zfill(8)
+    if format == "dec":
+        return bin(int(source, 10))
+    if format == "oct":
+        return bin(int(source, 8))
+    if format == "bin":
+        return '0b' + source
+
+def encodeFromBinary(binary, format):
+    if format == "raw":
+        print(f"binary {binary}")
+        n = int(binary, 2)
+        n = n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+        return n
+    if format == "b64":        
+        return base64.b64encode(binary).decode('ascii')
+    if format == "hex":
+        return hex(int(binary, 2))[2:]
+    if format == "dec":
+        return str(int(binary, 2))
+    if format == "oct":
+        return oct(int(binary, 2))[2:]
+    if format == "bin":
+        return binary[2:]
+
+
+def translate(source, fromType, toType):
+    temp = decodeToBinary(source, fromType)
+    print(f"SOURCE: {source}")
+    print(f"TEMP: {temp}")
+    return encodeFromBinary(temp, toType)
 
 # 'argparse' is a very useful library for building python tools that are easy
 # to use from the command line.  It greatly simplifies the input validation
@@ -28,11 +67,10 @@ f = sock.makefile()
 # 1 = First dashed line detected
 # 2 = [src] -> [answer] line
 # 3 = source text
-# 4 = 2nd dashed line detected
-# 5 = read for answer
+# 4 = 2nd dashed line detected. Send answer
 state = 0
 
-while True:
+while True:    
     line = f.readline().strip()
     # This iterates over data from the server a line at a time.  This can
     # cause some unexpected behavior like not seeing "prompts" until after
@@ -63,22 +101,19 @@ while True:
         state = 3
     elif state == 3:
         # take in source text         
-        print(f"CURRENT LINE: [{line}]")
+        print(f"SRC/FROM/TO: {line}, {currentFormat}, {wantedFormat}")
         answer = translate(line, currentFormat, wantedFormat)        
         # increment state
         state = 4
     elif state == 4:
         # confirm that we are on the dashed line
-        assert("-----" in line, f"Error: expected to be on dashed line. Currently on line: {line}")
-        # increment state
-        state = 5
-    elif state == 5:
-        # Send a response back to the server
-        answer = "Clearly not the answer..."
+        assert("-----" in line, f"Error: expected to be on dashed line. Currently on line: {line}")        
+        # Send a response back to the server                
         # The "\n" is important for the server's interpretation of your answer, so make
         # sure there is only one sent for each answer.        
+        print(f"Sending answer: {answer}")
         sock.send((answer + "\n").encode())         
         # Reset state
-        state = 0
-
+        state = 0 
+           
     
