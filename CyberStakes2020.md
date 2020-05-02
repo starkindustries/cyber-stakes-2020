ebc16590c63a43baef5ca3ce80360c6b5b631a86
Zion Perez <zionperez@gmail.com>
2020-05-01 23:24:00 -0600

    Solved kidsOnTheBlock added more notes. Added notes for NoEscape.

--- CyberStakes2020.md
+++ CyberStakes2020.md
@@ -407,3 +407,29 @@ INTERNAL_HOST=rootca.digisigner.local
 
 Poking around router firmware using Binwalk
 https://www.youtube.com/watch?v=46tEIFAp7gc
+
+
+## No Escape - Points: 60
+### Solve
+Since in-person events are currently banned, some magician we've never heard of is trying to sell us on the idea of a "digital" magic show where the magician logs in using an impossible password. For added assurances, one lucky audience member is able to login and see the hash of the password as proof the password is impossible. We're willing to bet the secret to this magic trick is not all that complicated. http://challenge.acictf.com:10952
+
+### Hints
+Inexperienced web application developers don't always esacpe/sanitize user inputs in there database query strings. This frequently allows SQL injection attacks that result in unintended behavior.
+The developer was pretty new, so just causing the query to error out may get you more information for the exploit. What happens when you use a single ' or " in each of the login fields?
+You'll need to login as a specific user. If you're new to SQL syntax, this might be useful resource for understanding the intended query and how you can manipulate it for your purposes.
+
+## Notes
+Enter a single quote (') in the username and password fields gives the following:
+'''
+SELECT username FROM users WHERE username = ''' AND pwHash = '265fda17a34611b1533d8a281ff680dc5791b0ce0a11c25b35e11c8e75685509'
+'''
+
+Looking at the 'pwHash', the hash is 64 characters long, which hints that it might be a sha256 hash. Using a sha256 tool to verify (https://xorbin.com/tools/sha256-hash-calculator), the sha256 hash of a single quote (') is:
+
+265fda17a34611b1533d8a281ff680dc5791b0ce0a11c25b35e11c8e75685509
+
+So the password field is definitely getting hashed. There is not much we can do with this field. However, the username field can be manipulated. Attempt to try the 1=1 trick:
+
+' or 1=1;--
+
+That worked. 
--- KidsOnTheBlock.js
+++ KidsOnTheBlock.js
@@ -219,3 +219,137 @@ function findContract() {
 // Solution
 // geth attach ./geth_data_dir/geth.ipc --exec 'loadScript("KidsOnTheBlock.js")' > transactions.txt
 getAllTransactions();
+
+
+// https://medium.com/@codetractio/inside-an-ethereum-transaction-fa94ffca912f
+// The first 32 bit a9059cbb is the first 32 bit of the hash of the function. In this case the function is transfer(address _to, uint256 _value) and its hash is
+// console.log(web3.sha3('transfer(address,uint256)'));
+// 0xa9059cbb2ab09eb219583f4a59a5d0623ade346d962bcd4e46b11da047c9049b
+
+// https://ethervm.io/decompile
+// This might be constructor bytecode - to get at the deployed contract, go back and remove the constructor prefix, usually up to the next 6060 or 6080.
+// Plug this into the decompiler
+// 60606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806312065fe014610051578063d0e30db014610077578063f3fef3a314610081575bfe5b341561005957fe5b6100616100c0565b6040518082815260200191505060405180910390f35b61007f610108565b005b341561008957fe5b6100be600480803573ffffffffffffffffffffffffffffffffffffffff16906020019091908035906020019091905050610158565b005b6000600060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000205490505b90565b34600060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825401925050819055505b565b80600060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054101515156101a75760006000fd5b80600060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff168152602001908152602001600020600082825403925050819055508173ffffffffffffffffffffffffffffffffffffffff166108fc829081150290604051809050600060405180830381858888f19350505050151561023457fe5b5b50505600a165627a7a72305820b81b4c88732e81c9348003df1a2cda8514646894b71981ae7efd91061bfb2c860029
+
+// And reveal this:
+// contract Contract {
+//     function main() {
+//         memory[0x40:0x60] = 0x60;
+//         var var0 = msg.data[0x00:0x20] / 0x0100000000000000000000000000000000000000000000000000000000 & 0xffffffff;
+    
+//         if (var0 == 0x12065fe0) {
+//             // Dispatch table entry for getBalance()
+//             if (msg.value) { assert(); }
+        
+//             var var1 = 0x0061;
+//             var1 = getBalance();
+//             var temp0 = memory[0x40:0x60];
+//             memory[temp0:temp0 + 0x20] = var1;
+//             var temp1 = memory[0x40:0x60];
+//             return memory[temp1:temp1 + (temp0 + 0x20) - temp1];
+//         } else if (var0 == 0xd0e30db0) {
+//             // Dispatch table entry for deposit()
+//             var1 = 0x007f;
+//             deposit();
+//             stop();
+//         } else if (var0 == 0xf3fef3a3) {
+//             // Dispatch table entry for withdraw(address,uint256)
+//             if (msg.value) { assert(); }
+        
+//             var1 = 0x00be;
+//             var var2 = msg.data[0x04:0x24] & 0xffffffffffffffffffffffffffffffffffffffff;
+//             var var3 = msg.data[0x24:0x44];
+//             withdraw(var2, var3);
+//             stop();
+//         } else { assert(); }
+//     }
+    
+//     function getBalance() returns (var r0) {
+//         memory[0x00:0x20] = msg.sender;
+//         memory[0x20:0x40] = 0x00;
+//         return storage[keccak256(memory[0x00:0x40])];
+//     }
+    
+//     function deposit() {
+//         memory[0x00:0x20] = msg.sender;
+//         memory[0x20:0x40] = 0x00;
+//         var temp0 = keccak256(memory[0x00:0x40]);
+//         storage[temp0] = storage[temp0] + msg.value;
+//     }
+    
+//     function withdraw(var arg0, var arg1) {
+//         memory[0x00:0x20] = msg.sender;
+//         memory[0x20:0x40] = 0x00;
+    
+//         if (storage[keccak256(memory[0x00:0x40])] < arg1) { revert(memory[0x00:0x00]); }
+    
+//         var temp0 = arg1;
+//         memory[0x00:0x20] = msg.sender;
+//         memory[0x20:0x40] = 0x00;
+//         var temp1 = keccak256(memory[0x00:0x40]);
+//         storage[temp1] = storage[temp1] - temp0;
+//         var temp2 = memory[0x40:0x60];
+//         var temp3;
+//         temp3, memory[temp2:temp2 + 0x00] = address(arg0 & 0xffffffffffffffffffffffffffffffffffffffff).call.gas(!temp0 * 0x08fc).value(temp0)(memory[temp2:temp2 + memory[0x40:0x60] - temp2]);
+    
+//         if (temp3) { return; }
+//         else { assert(); }
+//     }
+// }
+
+
+// The website reveals: 
+
+// Public Methods
+// Method names cached from 4byte.directory.
+// 0x12065fe0 getBalance()
+// 0xd0e30db0 deposit()
+// 0xf3fef3a3 withdraw(address,uint256)
+
+// 0xf3fef3a3 withdraw(address,uint256)
+// address: 000000000000000000000000c712e16f190368ee7b83faeeb1fc3b628a6917a4
+//   value: 00000000000000000000000000000000000000000000000077e772392b600000
+
+// 0xf3fef3a3
+// a: 00000000000000000000000050fc67693f00fbabc5473c3705ef057b09acf2c7
+// v: 00000000000000000000000000000000000000000000000022b1c8c1227a0000
+
+// ********************************
+// Deposit transactions with 2.5
+// 0xd0e30db0 deposit()
+// ********************************
+// tx hash          : 0x09c0dd28b28cf223ac4ad093808dc4ffc0461f7a512b4a0c5a566d98205084b5
+// nonce           : 4
+// blockHash       : 0x4fa6e84b34e4362eec6596112c46801e521fbaeee1f334d08e1ec1c92e19fd6a
+// blockNumber     : 48
+// transactionIndex: 2
+// from            : 0x4da56f7f58bc14c785cee861d25b2c417fe6853f ***************** 4TH HOP ADDRESS *****************
+// to              : 0x167f7969ae2ecf157306f798f63929903a02d771 ***************** 5TH HOP ADDRESS *****************
+// value           : 2.5
+// time            : 1571618645
+// gasPrice        : 1000000000
+// gas             : 3000000
+// input           : 0xd0e30db0
+
+// **************************************
+// Withdrawal transactions with 2.5
+// 0xf3fef3a3 withdraw(address,uint256)
+// **************************************
+// tx hash          : 0xf678663343298496dcb73f77c0500ec1f19ec94554a73b5495c651e3dc1e2629
+// nonce           : 5
+// blockHash       : 0xd7d938fcefbe821e1ed20b9a96b91e3e9efae9bbe637a1df9ca41d51a9fd1868
+// blockNumber     : 60
+// transactionIndex: 4
+// from            : 0x4da56f7f58bc14c785cee861d25b2c417fe6853f ***************** 4TH HOP ADDRESS *****************
+// to              : 0x167f7969ae2ecf157306f798f63929903a02d771 ***************** CONTRACT ADDRESS *****************
+// value           : 0
+// time            : 1571618764
+// gasPrice        : 1000000000
+// gas             : 3000000
+// input           : 0xf3fef3a300000000000000000000000050fc67693f00fbabc5473c3705ef057b09acf2c700000000000000000000000000000000000000000000000022b1c8c1227a0000
+
+// 0xf3fef3a3
+// a: 00000000000000000000000050fc67693f00fbabc5473c3705ef057b09acf2c7 = 0x50fc67693f00fbabc5473c3705ef057b09acf2c7
+// v: 00000000000000000000000000000000000000000000000022b1c8c1227a0000 = 2500000000000000000
+
+// 0x50fc67693f00fbabc5473c3705ef057b09acf2c7 is the 7th and FINAL address!
